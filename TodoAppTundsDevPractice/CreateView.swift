11951fcb1689d0e2dd0ab01b3797227a434e71ea
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-16 05:14:07 -0600

    -- Progress: Added modelContainer to the main app, and UI for creating a new todo Item, and a class for a simple todo item

---
+++ TodoAppTundsDevPractice/CreateView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateView.swift
+//  TodoAppTundsDevPractice
+//
+//  Created by Mitch Andrade on 10/16/23.
+//
+
+import SwiftUI
+
+struct CreateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateView()
+}
