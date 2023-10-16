ac54c43752f0df060d1adae65b6493d20e8f899d
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-16 05:00:44 -0600

    Initial Commit

---
+++ TodoAppTundsDevPractice/TodoAppTundsDevPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppTundsDevPracticeApp.swift
+//  TodoAppTundsDevPractice
+//
+//  Created by Mitch Andrade on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppTundsDevPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
