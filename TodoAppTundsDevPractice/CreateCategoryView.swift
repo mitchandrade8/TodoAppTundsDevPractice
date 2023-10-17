6a4265dde26ca5479e3e79289b6859e7cd768c15
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-17 14:26:42 -0600

    --Progress: Building out UI for Category and going over relationships: 1 - to many

---
+++ TodoAppTundsDevPractice/CreateCategoryView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateCategoryView.swift
+//  TodoAppTundsDevPractice
+//
+//  Created by Mitch Andrade on 10/17/23.
+//
+
+import SwiftUI
+
+struct CreateCategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateCategoryView()
+}
